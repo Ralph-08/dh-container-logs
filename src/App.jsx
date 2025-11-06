e3fb4f788cb67a6d3b6c2fb3195213c1ef7b28ef
Rafael Ramos <junior07.rrn@hotmail.com>
2025-11-05 19:59:01 -0500

    Create review page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LogsPage from "./pages/LogsPage/LogsPage";
 import LiveTrack from "./pages/LiveTrack/LiveTrack";
 import ContainerDetails from "./pages/ContainerDetails/ContainerDetails";
+import ReviewPage from "./pages/ReviewPage/ReviewPage";
 
 function App() {
   return (
@@ -11,7 +12,11 @@ function App() {
         <Routes>
           <Route path="/" element={<LogsPage />} />
           <Route path="/track" element={<LiveTrack />} />
-          <Route path="/container/:containerNumber" element={<ContainerDetails />} />
+          <Route path="/review" element={<ReviewPage />} />
+          <Route
+            path="/container/:containerNumber"
+            element={<ContainerDetails />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/ReviewPage/ReviewPage.jsx
@@ -0,0 +1,12 @@
+import "./ReviewPage.scss";
+
+const ReviewPage = () => {
+  return (
+    <div className="review-page">
+      <h1>Review Page</h1>
+      {/* Add your review page content here */}
+    </div>
+  );
+};
+
+export default ReviewPage;
